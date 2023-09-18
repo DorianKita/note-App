28bca80d1df2da6b642681ebadfe9f37c0ea5a92
Dorian Kita <dorian.kita90@gmail.com>
2023-09-18 19:37:34 +0200

    added function saveNote for saving notes on localStorage, and function createNote for displaying notes on page in section 'notes'

---
+++ app.js
@@ -0,0 +1,23 @@
+function saveNote() {
+    const textNote = document.querySelector('.note-area').value;
+    const keyNote = Date.now().toString();
+    localStorage.setItem(keyNote, textNote);
+    createNote(keyNote,textNote);
+}
+
+function deleteNote(keyNote){
+
+
+}
+
+function createNote(keyNote, textNote){
+    const div = document.createElement('div');
+    div.id = keyNote;
+    const text = document.createTextNode(textNote);
+    div.appendChild(text);
+
+    const button = document.createElement('button');
+    button.textContent ='Delete';
+    div.appendChild(button);
+    document.querySelector('.notes').appendChild(div);
+}
